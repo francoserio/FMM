737532e5047c7ed25ca372a38e95cea4dad95497
Franco Serio <francoagustinserio@gmail.com>
2018-01-18 00:38:57 -0300

    falta el upload en cuestion

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
+var bodyParser = require('body-parser')
+var path = require('path');
+var multer  = require('multer');
+var upload = multer();
+
 var express = require('express');
 
 var app = express();
 
 app.set('views', './views');
 
-router.get("/",function(req,res){
-  res.sendFile(path + "index.html");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, 'bower_components')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/", function(req,res){
+  res.sendFile(__dirname + '/views/home.html');
 });
 
-app.on('stormpath.ready',function(){
-  console.log('Stormpath Ready');
+app.listen(process.env.PORT || 3000, function() {
+  console.log("App is listening to port " + (process.env.PORT || 3000));
 });
-
-app.listen(3000);
